6824c7a466ba0470cceafb5e80b58e2f1e8bbab1
frederic <frederic.torcheux@djit.fr>
2017-04-17 11:31:11 +0200

    Restore native getExtractedDataMono

--- soundsystem/src/main/java/fr/bowserf/soundsystem/SoundSystem.java
+++ soundsystem/src/main/java/fr/bowserf/soundsystem/SoundSystem.java
@@ -145,8 +145,7 @@ public class SoundSystem {
      * @return A shorrt array containing mono data of extracted audio file.
      */
     public short[] getExtractedDataMono(){
-        //return native_get_extracted_data_mono();
-        return new short[0];
+        return native_get_extracted_data_mono();
     }
 
     /**
